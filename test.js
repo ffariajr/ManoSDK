44323d6acc9d55db9f9b3b49fc9042b2ead3ed40
Fernando Faria <fariaf1@tcnj.edu>
2015-05-13 00:09:23 -0400

    Initial Code

---
+++ test.js
@@ -0,0 +1,14 @@
+/*jslint evil: true */
+
+function generate() {
+    "use strict";
+
+    if (!document.getElementById("radio1-1").checked && !document.getElementById("radio1-2").checked) {
+        window.alert("Please choose an output option!");
+        return;
+    }
+    
+    
+
+
+}
